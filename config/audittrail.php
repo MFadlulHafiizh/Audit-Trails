e40f5c9514d15ae1c7ef1aa6f0754fdf1983055c
Fadlul Hafiizh <rrkuncen@gmail.com>
2023-09-07 22:12:14 +0700

    fix bug user id not saved on transaction log

---
+++ config/audittrail.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings
+    |--------------------------------------------------------------------------
+    |
+    | Set some default values. It is possible to add all defines that can be set
+    |
+    */
+    'userPrimaryKeyTable' => 'id',   // Set primary key for your users table
+);
--- src/LogTransaction.php
+++ src/LogTransaction.php
@@ -76,10 +76,7 @@ trait LogTransaction
         if ($action !== 'CREATE') {
             $oldValues = $model->getOriginal();
         }
-        $primaryUser = @Auth::user()->id;
-        if($model->getTable() == 'users'){
-            $primaryUser = @Auth::user()->{$model->getKeyName()};
-        }
+        $primaryUser = @Auth::user()->{Auth::user()->getKeyName()};
         self::replaceForeignValue($model, $oldValues);
         self::replaceForeignValue($model, $newValues);
         $logTable = new ActivityLog;
